f0112705e601a8ce63102f13aeeeb81957909b0f
Nishant Dixit <nishantpadit2004@gmail.com>
2026-07-13 03:04:08 +0530

    Replace template personal info with Stackwrights branding

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
         <div>
           <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto by Stackwrights Studio. A small studio building AI systems and automation that quietly handle the work your team shouldn't have to.</p>
         </div>
 
         <div>
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+91-8923827870</li>
-            <li>akshitsagar24@gmail.com</li>
+            <li>hello@stackwrights.com</li>
+            <li>stackwrights.com</li>
           </ul>
         </div>
 
@@ -33,7 +33,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ AKSHIT SINGH - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright © 2026 Stackwrights Studio - All Rights Reserved.</p>
       </div>
 
     </div>
